343de43e2416bc27913a255b6e1635d173760360
Jesse Ekoh-Ordan <jesseekoh@gmail.com>
2025-07-30 14:11:21 +0100

    Add Gemfile and Gemfile.lock for dependency management; implement main game loop

---
+++ main.rb
@@ -0,0 +1,16 @@
+require_relative 'board'
+require_relative 'player'
+require_relative 'game'
+
+player1 = Player.new('Jesse', 'X')
+player2 = Player.new('Solomon', 'O')
+
+game = Game.new(player1, player2)
+
+until game.over?
+  game.board.display
+  puts "it's #{game.current_player.name}'s turn"
+  puts 'Enter your move'
+  row, col = gets.chomp.split.map(&:to_i)
+  game.play_turn(row, col)
+end
