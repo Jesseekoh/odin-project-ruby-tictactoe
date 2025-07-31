8e29486a6d3e930b31ca602616a4009e8967c2de
Jesse Ekoh-Ordan <jesseekoh@gmail.com>
2025-07-31 20:10:37 +0100

    Refactor Board class methods for clarity and consistency; update grid initialization and winner checks

--- board.rb
+++ board.rb
@@ -1,34 +1,59 @@
+# frozen_string_literal: true
+
+# Represents the TicTacToe game board, handles move logic and checks for winner
 class Board
   def initialize
-    @grid = Array.new(3) { Array.new(3, '_') }
+    @grid = Array.new(3) { Array.new(3, '') }
   end
 
+  # Print's out the game board
   def display
-    @grid.each { |row| puts row.join('|') }
+    @grid.each_with_index do |row, row_index|
+      row.each_with_index do |cell, col_index|
+        print(row_index * 3 + col_index) if cell == ''
+        print cell unless cell == ''
+        print ' | ' unless col_index == row.size - 1
+      end
+      puts
+      puts '----------' unless row_index == @grid.size - 1
+    end
   end
 
   def make_move(row, col, symbol)
     @grid[row][col] = symbol
   end
 
+  # Checks if there's a winner
   def winner?
-    # check horizontal
-    if horizontal_win?
-      puts 'A row has a winner'
-    elsif vertical_win
-      puts 'There is a vertical winner'
+    return false unless horizontal_win? || vertical_win?
+
+    puts 'We have a winner'
+    true
+  end
+
+  # checks if a move is valid
+  def valid_move?(row, col)
+    @grid[row][col] == ''
+  end
+
+  # checks the board for horizontal wins
+  def horizontal_win?
+    @grid.any? { |row| row.all? { |cell| cell != '' } }
+  end
+
+  # checks the board for vertical wins
+  def vertical_win?
+    if (
+      @grid[0][0] != '' && @grid[0][0] == @grid[1][0] &&
+      @grid[0][0] == @grid[2][0]) ||
+       (@grid[0][1] != '' && @grid[0][1] == @grid[1][1] &&
+        @grid[0][1] == @grid[2][1]) || (@grid[0][2] != '' && @grid[0][2] == @grid[1][2] && @grid[0][2] == @grid[2][2])
+      true
     end
   end
 
-  def horizontal_win?
-    @grid.any? { |row| row.all? { |cell| cell != '_' } }
-  end
-
-  def vertical_win
-    @grid[0][0] != '' && @grid[0][0] == @grid[1][0] && @grid[0][0] == @grid[2][0]
-  end
-
-  def is_full?
-    @grid.all? { |row| row.all? { |cell| cell != '_' } }
+  # Checks if all cells of the board are filled up
+  def full?
+    @grid.all? { |row| row.all? { |cell| cell != '' } }
   end
 end
